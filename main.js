ddb4405d1a705d35ea9edf5496958d5561b567b7
ViniciusCelestino078 <132959683+ViniciusCelestino078@users.noreply.github.com>
2025-03-19 12:12:16 -0300

    index.html

--- main.js
+++ main.js
@@ -27,11 +27,33 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo1 = new Date("2020-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2023-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 let tempoAtual = new Date();
 
+if (tempoFinal > 0){
+    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+} else {
+    return "Prazo Finalizado";
+}
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function atualizaCronometro(){
+    for (let i=0; i<contadores.length;i++){
+        contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+}
+function comecaCronometro(){
+    atualizaCronometro();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
@@ -42,3 +64,29 @@ function calculaTempo(tempoObjetivo) {
     let dias = Math.floor(horas / 24);
     return dias;
   }
+  let dias = Math.floor(horas / 24);
+  for (let i=0; i<contadores.length;i++){
+    // contadores[i].textContent = calculaTempo(tempos[i]);   
+    }
+    if (tempoFinal > 0){
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return [0,0,0,0];
+    }
+    function atualizaCronometro(){
+        document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+        document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];
+        document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+        document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
+    
+        for (let i=0; i<contadores.length;i++){
+            document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
+            console.log("seg"+i);
+        }
+    }
+        if (tempoFinal > 0){
+            return [dias,horas,minutos,segundos];
+        } else {
+            return [0,0,0,0];
+        }
+        
